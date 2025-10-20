6961cbbe6eeeed34657147839968b37b90f90319
Alejoso <atirador1@eafit.edu.co>
2025-10-19 21:45:24 -0500

    Added shooting

---
+++ Assets/Scripts/BulletScript.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    private Vector3 mousePos;
+    private Camera cam;
+    private Rigidbody2D rb;
+    public float speed;
+    void Start()
+    {
+        //Get mouse position
+        cam = Camera.main;
+        rb = GetComponent<Rigidbody2D>();
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+
+        //Get the direction and rotation the bullet is going to be shot
+        Vector3 direction = mousePos - transform.position;
+        Vector3 rotation = transform.position - mousePos;
+
+        //Apply velocity in that direction and apply the corresponding rotation
+        rb.linearVelocity = new Vector2(direction.x, direction.y).normalized * speed;
+        float rot = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, rot + 90); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,11 +3,20 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
+    //Player variables
     [SerializeField]
     private float speed = 5f;
 
     Rigidbody2D rb;
     Camera cam;
+    public float lookingDirection;
+
+
+    //Shooting mecanic variables
+    public GameObject bullet; 
+    public bool canShoot = true;
+    public float shootingTimer;
+    public float timeBetweenShooting; 
 
 
     void Awake()
@@ -20,7 +29,8 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate()
     {
         MovePlayer();
-        RotateSprite(); 
+        RotateSprite();
+        Shooting(); 
     }
 
     void MovePlayer()
@@ -36,19 +46,39 @@ public class PlayerController : MonoBehaviour
 
         rb.linearVelocity = input * speed; // constant speed in any direction
     }
-    
+
     void RotateSprite()
     {
         // --- Mirar hacia el mouse ---
         Vector3 mouseWorld = cam.ScreenToWorldPoint(Input.mousePosition);
-        Vector2 toMouse = (mouseWorld - transform.position);
-        float angleDeg = Mathf.Atan2(toMouse.y, toMouse.x) * Mathf.Rad2Deg;
+        Vector2 toMouse = mouseWorld - transform.position;
+        lookingDirection = Mathf.Atan2(toMouse.y, toMouse.x) * Mathf.Rad2Deg;
 
         // Ajusta este offset según cómo esté dibujado tu sprite:
         // si "mira" hacia ARRIBA en el arte, usa -90f; si mira a la DERECHA, usa 0f.
         float spriteOffset = -90f;
 
-        rb.MoveRotation(angleDeg + spriteOffset);
+        rb.MoveRotation(lookingDirection + spriteOffset);
+
+    }
+    
+    void Shooting()
+    {
+        if (canShoot == false)
+        {
+            shootingTimer += Time.deltaTime;
+            if (shootingTimer >= timeBetweenShooting)
+            {
+                canShoot = true;
+                shootingTimer = 0;
+            }
+        }
+        
+        if(Input.GetMouseButton(0) && canShoot)
+        {
+            canShoot = false;
+            Instantiate(bullet, transform.position, Quaternion.identity); 
+        }
     }
     
 }
